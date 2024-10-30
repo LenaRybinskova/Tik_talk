e666f493633f9ebdbfb430d46b0b95a236197875
«Elena <LenaRybinskova@gmail.com>
2024-10-30 22:07:26 +0300

    sidebar SVG

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {SvgIconComponent} from '../svg-icon/svg-icon.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, SvgIconComponent],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
 
+  menuItems=[
+    {label:"Моя страница", icon:"home",link:""},
+    {label:"Чаты", icon:"chat",link:""},
+    {label:"Поиск", icon:"search",link:""},
+  ]
 }
