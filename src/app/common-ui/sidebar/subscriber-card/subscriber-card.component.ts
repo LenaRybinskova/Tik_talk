aa83b93d4551e401e2ef6a817b460694ed2a704c
«Elena <LenaRybinskova@gmail.com>
2024-10-31 11:22:36 +0300

    sidebarPage: component Suncribers

---
+++ src/app/common-ui/sidebar/subscriber-card/subscriber-card.component.ts
@@ -0,0 +1,13 @@
+import { Profile } from './../../../data/interfaces/profile.interface';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-subscriber-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './subscriber-card.component.html',
+  styleUrl: './subscriber-card.component.scss',
+})
+export class SubscriberCardComponent {
+  @Input() profile!: Profile;
+}
